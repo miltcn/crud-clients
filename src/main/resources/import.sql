91ec3ae5f0de4539800c7d6402ce0b0592d7cf4f
Milton <milton.cneto@gmail.com>
2022-02-22 10:17:59 -0300

    Seed clients H2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Tanjiro Kamado', '58583281050', 2000, TIMESTAMP WITH TIME ZONE '2004-07-14T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Nezuko Kamado', '51537447017', 1500, TIMESTAMP WITH TIME ZONE '1999-08-10T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Zenitsu Agatsuma', '96687492004', 6650, TIMESTAMP WITH TIME ZONE '2000-01-21T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Inosuke Hashibira', '71504030001', 1800, TIMESTAMP WITH TIME ZONE '2001-12-31T10:00:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Kanao Tsuyuri', '28408348043', 500.5, TIMESTAMP WITH TIME ZONE '2014-10-01T10:00:00Z', 0);
+
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Levi Ackerman', '50693100036', 10000, TIMESTAMP WITH TIME ZONE '1995-12-14T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Eren Yeager', '92455710092', 1000, TIMESTAMP WITH TIME ZONE '2005-03-14T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Mikasa Ackerman', '98896393000', 3000, TIMESTAMP WITH TIME ZONE '2004-02-14T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Historia Reiss', '51052875092', 50000, TIMESTAMP WITH TIME ZONE '2006-01-14T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Annie Leonhart', '14248162032', 5000, TIMESTAMP WITH TIME ZONE '2003-03-14T10:00:00Z', 0);
